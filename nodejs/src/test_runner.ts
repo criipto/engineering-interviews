5efd4cb5f7dca0924911d11f37540140898b33bd
Mick Hansen <maker@mhansen.io>
2024-04-22 10:19:25 +0200

    add process.nextTick clue

--- nodejs/src/test_runner.ts
+++ nodejs/src/test_runner.ts
@@ -1,3 +1,4 @@
+import process from 'node:process';
 
 export function describe(name: string, fn: () => void) {
 
@@ -6,3 +7,7 @@ export function describe(name: string, fn: () => void) {
 export function it(name: string, fn: () => void | Promise<void>) {
   
 }
+
+process.nextTick(() => {
+  console.log('A clue');
+});
